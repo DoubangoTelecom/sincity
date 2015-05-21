b27bb6ba9d8b1f27942e6a4475a4798a12cfda18
BobHournbuckleGE <BobHournbuckleGE@gmail.com>
2015-05-21 00:07:59 +0000

    Complete the removal of wcecompat

--- thirdparties/wince/scripts/autogen-mentor-x86-wcecompat.bat
+++
@@ -1,7 +0,0 @@
-set OSVERSION=WCE700
-set TARGETCPU=X86
-set PLATFORM=VC-CE
-set INCLUDE=C:\Program Files (x86)\Windows CE Tools\SDKs\GE vPC WEC7 SDK\Include\X86
-set LIB=C:\Program Files (x86)\Windows CE Tools\SDKs\GE vPC WEC7 SDK\Lib\x86
-perl config.pl
-nmake
