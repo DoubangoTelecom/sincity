f0174e89136c360451830a70cda0968f3adee5e0
thialgou <thialgou@gmail.com>
2015-06-20 06:21:03 +0000

    Use MAC address calc function from tinyNET (requires Doubango 1311)

--- sincity/src/sc_utils.cc
+++ sincity/src/sc_utils.cc
@@ -1,8 +1,6 @@
 #include "sincity/sc_utils.h"
 
 #if SC_UNDER_WINDOWS
-#	include <windows.h>
-#	include <iphlpapi.h>
 #	include "tinydav/tdav_win32.h"
 #elif SC_UNDER_APPLE
 #	include "tinydav/tdav_apple.h"
@@ -12,6 +10,8 @@
 #include "tsk_memory.h"
 #include "tsk_uuid.h"
 #include "tsk_plugin.h"
+
+#include "tnet_utils.h" /* tnet_get_mac_address */
 
 std::string SCUtils::randomString()
 {
@@ -46,31 +46,16 @@ std::string SCUtils::macAddress()
 {
 	static std::string __mac_address = "";
 	if (__mac_address.empty()) {
-#if SC_UNDER_WINDOWS
-		IP_ADAPTER_INFO *info = NULL, *pos;
-		DWORD size = 0;
-		ULONG ret;
-		char* address = tsk_null;
-
-		if ((ret = GetAdaptersInfo(info, &size)) == ERROR_SUCCESS || ret == ERROR_BUFFER_OVERFLOW) {
-			info = (IP_ADAPTER_INFO *)tsk_calloc(size + 1, 1);
-			if (info) {
-				if ((ret = GetAdaptersInfo(info, &size)) == ERROR_SUCCESS) {
-					for (pos = info; pos != NULL && address == tsk_null; pos = pos->Next) {
-						if (pos->Type == MIB_IF_TYPE_LOOPBACK && pos->Next) { // skip loopback if we still have items to check
-							continue;
-						}
-						for (UINT i = 0; i < pos->AddressLength; ++i) {
-							tsk_strcat_2(&address, "%2.2x", pos->Address[i]);
-						}
-					}
-				}
+		tnet_mac_address address_bytes = { 0 };
+		char* address_str = tsk_null;
+		int ret = tnet_get_mac_address(&address_bytes);
+		if (ret == 0) {
+			for (tsk_size_t i = 0; i < sizeof(address_bytes) / sizeof(address_bytes[0]); ++i) {
+				tsk_strcat_2(&address_str, "%2.2x", address_bytes[i]);
 			}
-			__mac_address = std::string(address);
-			TSK_FREE(info);
-			TSK_FREE(address);
+			__mac_address = std::string(address_str);
+			TSK_FREE(address_str);
 		}
-#endif
 	}
 	return __mac_address;
 }
